5c2f15eff2270b64a866bd4e91b8725e6d2fb177
Tobin Chew <tobin@smarterclick.com>
2019-12-11 09:49:28 +0000

    1.1.0

--- src/tests/chunk.test.ts
+++ src/tests/chunk.test.ts
@@ -1,25 +1,20 @@
-import { chunker } from '../index'
+import { chunker } from "../index";
 
-test('Plain array', () => {
-    const input = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-    ];
-    let output = chunker<typeof input[0]>(input, 3)
-    let expected = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [10, 11]
-    ]
-    expect(output).toEqual(expected);
-})
+test("Plain array", () => {
+  const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  let output = chunker<typeof input[0]>(input, 3);
+  let expected = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11]
+  ];
+  expect(output).toEqual(expected);
+});
 
-
-test('Input type error', () => {
-    const input = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-    ];
-    expect(() => {
-        chunker<typeof input[0]>(input, -1)
-    }).toThrow(TypeError)
-})
+test("Input type error", () => {
+  const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  expect(() => {
+    chunker<typeof input[0]>(input, -1);
+  }).toThrow(TypeError);
+});
